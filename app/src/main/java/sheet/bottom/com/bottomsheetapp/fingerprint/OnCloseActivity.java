7d30367a0ac39b12690fd4ff4d8a88a78e3606cc
Battula, Lakshmana Rao (US - Bengaluru) <lakshmanraob@gmail.com>
2017-04-26 15:06:17 +0530

    commit to remove finger print

--- app/src/main/java/sheet/bottom/com/bottomsheetapp/fingerprint/OnCloseActivity.java
+++
@@ -1,9 +0,0 @@
-package sheet.bottom.com.bottomsheetapp.fingerprint;
-
-/**
- * Created by labattula on 09/12/16.
- */
-
-public interface OnCloseActivity {
-    public void closeCurrentActivity();
-}
