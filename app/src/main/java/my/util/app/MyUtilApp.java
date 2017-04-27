82f6972e8921335c18a121ba77da07288356690d
Battula, Lakshmana Rao (US - Bengaluru) <lakshmanraob@gmail.com>
2017-04-27 12:15:14 +0530

    Removed unwanted imports
    addition of LoginFragment screen
    Colors inclusion
    Strings update with hint and help

--- app/src/main/java/my/util/app/MyUtilApp.java
+++ app/src/main/java/my/util/app/MyUtilApp.java
@@ -2,26 +2,17 @@ package my.util.app;
 
 import android.app.Application;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.Volley;
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
 import com.joanzapata.iconify.fonts.MaterialModule;
 
-import butterknife.ButterKnife;
-import my.util.app.utils.LruBitmapCache;
-
-/**
+/*
  * Created by labattula on 23/09/16.
  */
 
 public class MyUtilApp extends Application {
     public static final String TAG = MyUtilApp.class
             .getSimpleName();
-
-    private RequestQueue mRequestQueue;
-    private ImageLoader mImageLoader;
 
     private static MyUtilApp mInstance;
 
@@ -35,22 +26,6 @@ public class MyUtilApp extends Application {
 
     public static synchronized MyUtilApp getInstance() {
         return mInstance;
-    }
-
-    public RequestQueue getRequestQueue() {
-        if (mRequestQueue == null) {
-            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
-        }
-        return mRequestQueue;
-    }
-
-    public ImageLoader getImageLoader() {
-        getRequestQueue();
-        if (mImageLoader == null) {
-            mImageLoader = new ImageLoader(this.mRequestQueue,
-                    new LruBitmapCache());
-        }
-        return this.mImageLoader;
     }
 
 }
